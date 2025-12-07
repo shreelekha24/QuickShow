7578243f598119901346483bfc0cfe6eee439dc8
shreelekha24 <2023ugee027@nitjsr.ac.in>
2025-12-07 21:00:10 +0530

    change

--- server/models/Show.js
+++ server/models/Show.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
-const showSchema= new mongoose.Schema(
-    {
-        movie:{type: String, required:true, ref: 'Movie'},
-        showDateTime:{type: Date, required:true},
-        showPrice:{type: Number, required:true},
-        occupiedSeats:{type: Object, default:{}}
-    },{minimize:false}
-)
+const showSchema = new mongoose.Schema(
+  {
+    movie: { type: String, required: true, ref: 'Movie' },
+    showDateTime: { type: Date, required: true },
+    showPrice: { type: Number, required: true },
+    occupiedSeats: { type: Object, default: {} },
+    isActive: { type: Boolean, default: true } // ✅ ADD THIS
+  },
+  { minimize: false, timestamps: true }
+);
+
 
 const Show=mongoose.model('Show',showSchema)
 
